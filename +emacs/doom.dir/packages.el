6006f7e541eadaa0fbdd95ba5495a8e07534336e
Blue Rose <mail@bluerose.garden>
2025-02-18 20:12:02 -0600

    packages

--- +emacs/doom.dir/packages.el
+++ +emacs/doom.dir/packages.el
@@ -48,3 +48,8 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+
+ (package! rustowlsp
+   :recipe (:host github :repo "cordx56/rustowl"
+            :files (:defaults "emacs/*")))
