76a51b272ec45afc10a40878b7ca55476d28dc3b
Blue Rose <mail@bluerose.garden>
2025-11-19 14:37:15 -0600

    add obsidian to magic

--- +emacs/magic.doom.dir/config.el
+++ +emacs/magic.doom.dir/config.el
@@ -223,3 +223,27 @@
   :config
   (add-to-list 'eglot-server-programs
                '(text-mode . ("harper-ls" "--stdio"))))
+
+(require 'obsidian)
+;; Location of obsidian vault
+(setopt obsidian-directory "~/+Vaults/BabaYaga/")
+;; Default location for new notes from `obsidian-capture'
+(setopt obsidian-inbox-directory "☆Inbox")
+;; Useful if you're going to be using wiki links
+(setopt markdown-enable-wiki-links t)
+
+;; These bindings are only suggestions; it's okay to use other bindings
+;; Create note
+;;(define-key obsidian-mode-map (kbd "C-c C-n") 'obsidian-capture)
+;; If you prefer you can use `obsidian-insert-wikilink'
+;;(define-key obsidian-mode-map (kbd "C-c C-l") 'obsidian-insert-link)
+;; Open file pointed to by link at point
+;;(define-key obsidian-mode-map (kbd "C-c C-o") 'obsidian-follow-link-at-point)
+;; Open a note note from vault
+;;(define-key obsidian-mode-map (kbd "C-c C-p") 'obsidian-jump)
+;; Follow a backlink for the current file
+;;(define-key obsidian-mode-map (kbd "C-c C-b") 'obsidian-backlink-jump)
+
+;; Activate obsidian mode and backlinks mode
+(global-obsidian-mode t)
+(obsidian-backlinks-mode t)
--- +emacs/magic.doom.dir/packages.el
+++ +emacs/magic.doom.dir/packages.el
@@ -59,3 +59,5 @@
 (package! org-tracktable)
 (package! valign)
 (package! ftable)
+(package! obsidian)
+
